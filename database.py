43abef20304e922cf94759dc630ef7e87c58c86d
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2019-01-26 21:24:20 +0530

    Updated database management script in SQLite3

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+import time
+from datetime import datetime
+import random
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('fivethirtyeight')
+
+class Data:
+    def __init__(self):
+        self.conn=sqlite3.connect("data.db")
+        self.c=self.conn.cursor()
+        self.c.execute("CREATE TABLE IF NOT EXISTS users(unix REAL, datestamp TEXT, username TEXT, password TEXT)")
+
+    def create_table(self, name):
+        query="CREATE TABLE IF NOT EXISTS {}(unix REAL, datestamp TEXT, username TEXT, password TEXT, key TEXT)".format(name)
+        self.c.execute(query)
+
+    def check_table(self, name, username, password):
+        self.c.execute("SELECT * FROM {}".format(name))
+        for row in self.c.fetchall():
+            if row[2]==username and row[3]==password:
+                return True
+        return False
+
+    def read_table(self, name):
+        self.c.execute("SELECT * FROM {}".format(name))
+        for row in self.c.fetchall():
+            print(row)
+
+    def user_entry(self, username, password):
+        unix_time=time.time()
+        date=datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+        self.c.execute("INSERT INTO users (unix, datestamp, username, password) VALUES (?, ?, ?, ?)",(unix_time, date, username, password))
+        self.conn.commit()
+
+    def group_entry(self, group_name, username, password, key):
+        unix_time=time.time()
+        date=datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+        self.c.execute("INSERT INTO {} (unix, datestamp, username, password, key) VALUES (?, ?, ?, ?, ?)".format(group_name),(unix_time, date, username, password, key))
+        self.conn.commit()
+
+if __name__=="__main__":
+    obj=Data()
+    obj.read_table("users")
+    obj.read_table("ABC")
