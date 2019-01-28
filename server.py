9dc64293e0bca36c9c0d68da38830575006ffc78
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2019-01-28 20:22:16 +0530

    Updated server script to hash the password while storing into the database.

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 import asyncio
 import random
 from database import Data
+from rsa import RSA
+from Crypto.Hash import SHA256
 
 class Server:
     def __init__(self, host, port):
         self.host=host
         self.port=port
+        self.crypt=RSA()
+        tuple=self.crypt.prime_tuple()
+        p, q = tuple
+        n=p*q
+        e=self.crypt.public_key(tuple)
+        self.pub_k=(e, n)
+        d=self.crypt.private_key(tuple, e)
+        self.priv_k=(d, n)
 
     async def start(self, loop):
         serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +36,7 @@ class Server:
 
     async def handler(self, conn, loop):
         obj=Data()
+        hash=SHA256.new()
         username=""
         password=""
         public_key=""
@@ -36,9 +47,9 @@ class Server:
             username=res
         await loop.sock_sendall(conn, str.encode("Enter password: "))
         res_en=await loop.sock_recv(conn, 2048)
-        res=res_en.decode("utf-8")
-        if res:
-            password=res
+        if res_en:
+            hash.update(res_en)
+            password=hash.digest()
 
         result=obj.check_table("users", username, password)
         if not result:
@@ -48,7 +59,10 @@ class Server:
         res_en=await loop.sock_recv(conn, 2048)
         res=res_en.decode("utf-8")
         if res:
-            public_key=res
+            try:
+                public_key=eval(res)
+            except Exception as e:
+                print(str(e))
 
         await loop.sock_sendall(conn, str.encode("Name a group that you would want to create or join: "))
         res_en=await loop.sock_recv(conn, 2048)
@@ -56,7 +70,8 @@ class Server:
         if res is not None:
             obj.create_table(res)
             key=random.randint(100000, 1000000)
-            await loop.sock_sendall(conn, str.encode(str(key)))
+            key=self.crypt.encrypt(str(key), public_key)
+            await loop.sock_sendall(conn, str.encode(key))
             result=obj.check_table(res, username, password)
             if not result:
                 obj.group_entry(res, username, password, key)
